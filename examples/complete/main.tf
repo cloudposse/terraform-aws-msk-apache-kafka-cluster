609ba672fa1a1a0603b7ffd1f2f74071037a7dec
Hugo Samayoa <htplbc@gmail.com>
2020-09-04 16:06:34 -0700

    context updates

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,34 +3,24 @@ provider "aws" {
 }
 
 module "vpc" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.16.2"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  environment = var.environment
-  stage       = var.stage
-  name        = var.name
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
+  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.17.0"
+
   cidr_block  = "172.16.0.0/16"
+
+  context = module.this.context
 }
 
 module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.27.0"
-  enabled              = var.enabled
-  namespace            = var.namespace
-  environment          = var.environment
-  stage                = var.stage
-  name                 = var.name
-  delimiter            = var.delimiter
-  attributes           = var.attributes
-  tags                 = var.tags
+  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.28.0"
+
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
   nat_gateway_enabled  = true
   nat_instance_enabled = false
+
+  context = module.this.context
 }
 
 resource "random_id" "config_id" {
@@ -40,14 +30,7 @@ resource "random_id" "config_id" {
 
 module "kafka" {
   source                 = "../../"
-  enabled                = var.enabled
-  namespace              = var.namespace
-  environment            = var.environment
-  stage                  = var.stage
-  name                   = "${var.name}${var.delimiter}${try(random_id.config_id[0].hex, "")}"
-  delimiter              = var.delimiter
-  attributes             = var.attributes
-  tags                   = var.tags
+
   zone_id                = var.zone_id
   security_groups        = [module.vpc.vpc_default_security_group_id]
   vpc_id                 = module.vpc.vpc_id
@@ -55,4 +38,7 @@ module "kafka" {
   kafka_version          = var.kafka_version
   number_of_broker_nodes = var.number_of_broker_nodes
   broker_instance_type   = var.broker_instance_type
+
+  name    = "${var.name}${var.delimiter}${try(random_id.config_id[0].hex, "")}"
+  context = module.this.context
 }
--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,10 +1,18 @@
 terraform {
-  required_version = ">= 0.13.0, < 0.14.0"
+  required_version = ">= 0.13.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0, < 4.0"
+      version = ">= 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.3"
     }
   }
 }
