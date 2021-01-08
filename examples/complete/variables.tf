ad4a7a7f403938d507c946e3215d3c2baf9fe62e
Nicholas Hawkes <nicholas.hawkes@prodigygame.com>
2021-01-08 04:44:26 -0500

    Typo: Duplicated description text (#3)
    * Typo: Duplicated description text
    
    The `number_of_broker_nodes` in the example has duplicated text.
    
    * Updated README.md
    
    * Duplicated description text fix
    
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>
    Co-authored-by: Maxim Mironenko <maxim@cloudposse.com>

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -20,7 +20,7 @@ variable "broker_instance_type" {
 
 variable "number_of_broker_nodes" {
   type        = number
-  description = "The desired total number of broker nodes in the kafka cluster. It must be a multiple of the number of specified client subnets. It must be a multiple of the number of specified client subnets."
+  description = "The desired total number of broker nodes in the kafka cluster. It must be a multiple of the number of specified client subnets."
 }
 
 variable "region" {
