c5cb09c2e8d0ffe3b68f6de3c2f01010c3c38f82
Callum <callumccr@gmail.com>
2021-04-18 15:23:32 +0100

    decoupled kafka topics & msk cluster as it's a dependency of the provider config

--- provider.tf
+++ provider.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-  version             = "~> 2.0"
-  region              = var.aws_region
+  version = "~> 2.0"
+  region  = var.aws_region
   assume_role {
     role_arn = var.aws_assume_role_arn
   }
+}
+
+provider "kafka" {
+  bootstrap_servers = aws_msk_cluster.default.*.bootstrap_brokers != [] ? aws_msk_cluster.default.*.bootstrap_brokers : var.bootstrap_servers
 }
