58a722fb937b72c0736c1f40b5116be4ffda8d8a
nitrocode <nitrocode@users.noreply.github.com>
2021-11-07 21:54:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   enabled = module.this.enabled
 
-  brokers = flatten(data.aws_msk_broker_nodes.default.node_info_list.*.endpoints)
+  brokers = flatten(data.aws_msk_broker_nodes.default.*.node_info_list.*.endpoints)
   # If var.storage_autoscaling_max_capacity is not set, don't autoscale past current size
   broker_volume_size_max = coalesce(var.storage_autoscaling_max_capacity, var.broker_volume_size)
 
