fbc524ce17afdf6dfc1e1490797f1caba44bdb8d
Mohinish <mohnishbasha@users.noreply.github.com>
2022-09-03 01:36:50 -0500

    Patch storage_info object
    Patch storage_info object since ebs_volume_size is deprecated.

--- main.tf
+++ main.tf
@@ -186,7 +186,7 @@ resource "aws_msk_cluster" "default" {
   lifecycle {
     ignore_changes = [
       # Ignore changes to ebs_volume_size in favor of autoscaling policy
-      broker_node_group_info[0].ebs_volume_size,
+      broker_node_group_info[0].storage_info,
     ]
   }
 
