bf443142762d283457cef336ee2721b1c1422443
Douglas Brand-Williamson <dougbrw@gmail.com>
2023-04-18 20:06:51 +0100

    Make scram secret assocation optional (#78)
    * Make scram secret assocation optional
    
    * Auto Format
    
    ---------
    
    Co-authored-by: dougbw <douglas.brand-williamson@underwriteme.co.uk>
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -209,7 +209,7 @@ resource "aws_msk_cluster" "default" {
 }
 
 resource "aws_msk_scram_secret_association" "default" {
-  count = local.enabled && var.client_sasl_scram_enabled ? 1 : 0
+  count = local.enabled && var.client_sasl_scram_enabled && var.client_sasl_scram_secret_association_enabled ? 1 : 0
 
   cluster_arn     = aws_msk_cluster.default[0].arn
   secret_arn_list = var.client_sasl_scram_secret_association_arns
