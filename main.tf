14aedbe6e85a9d53638e1b525186f9df86c6db24
Šarūnas Navickas <sarunas.navickas@ovoko.com>
2023-04-24 16:10:23 +0300

    Code review changes

--- main.tf
+++ main.tf
@@ -134,9 +134,10 @@ resource "aws_msk_cluster" "default" {
     }
     client_subnets  = var.subnet_ids
     security_groups = var.create_security_group ? concat(var.associated_security_group_ids, [module.broker_security_group.id]) : var.associated_security_group_ids
+
     connectivity_info {
-      public_access {
-        type = var.public_access ? "SERVICE_PROVIDED_EIPS" : "DISABLED"
+      public_access_enabled {
+        type = var.public_access_enabled ? "SERVICE_PROVIDED_EIPS" : "DISABLED"
       }
     }
   }
--- variables.tf
+++ variables.tf
@@ -209,7 +209,7 @@ variable "storage_autoscaling_disable_scale_in" {
   description = "If the value is true, scale in is disabled and the target tracking policy won't remove capacity from the scalable resource."
 }
 
-variable "public_access" {
+variable "public_access_enabled" {
   type        = bool
   default     = false
   description = "Enable public access to MSK cluster (given that all of the requirements are met)"
