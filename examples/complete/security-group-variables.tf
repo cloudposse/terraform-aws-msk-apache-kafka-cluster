b8707fdc921b09bfdc62be51e77ecf44439ed3a3
aknysh <andriy.knysh@gmail.com>
2023-05-03 10:02:18 -0400

    updates

--- examples/complete/security-group-variables.tf
+++ examples/complete/security-group-variables.tf
@@ -172,14 +172,6 @@ variable "additional_security_group_rules" {
 # - It is difficult to document and easy to make mistakes using it
 
 
-## vpc_id is required, but if needed for reasons other than the security group,
-## it should be defined in the main `variables.tf` file, not here.
-variable "vpc_id" {
-  type        = string
-  description = "The ID of the VPC where the Security Group will be created."
-}
-
-
 #
 #
 #### The variables below (but not the outputs) can be omitted if not needed, and may need their descriptions modified
