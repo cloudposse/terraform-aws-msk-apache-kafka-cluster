bfeb91dd39f7d1a8698d5f259e1f034de715f4d1
Igor Rodionov <goruha@gmail.com>
2024-07-01 21:53:18 +0300

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -61,6 +61,8 @@ module "kafka" {
   certificate_authority_arns = var.certificate_authority_arns
 
   ## Use custom broker DNS name to avoid resource conflict for concurrent test runs
+  ## We use `%%%%ID%%%%` because format will replace it with `%%ID%%` 
+  ## that is expected placeholder for cluster node number in `custom_broker_dns_name`
   custom_broker_dns_name = format("msk-test-broker-%s-%%%%ID%%%%", var.attributes[0])
 
   context = module.this.context
