b1683af85ff4033f490de1d2f6940d8c66e8b404
Igor Rodionov <goruha@gmail.com>
2024-07-01 21:10:19 +0300

    Update examples_complete_test.go

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-  "fmt"
 	"regexp"
 	"strings"
 	"testing"
@@ -17,7 +16,6 @@ func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
 	randID := strings.ToLower(random.UniqueId())
 	attributes := []string{randID}
-  dns_name := fmt.Sprintf("msk-test-broker-%s-%%ID%%", randID)
 
 	rootFolder := "../../"
 	terraformFolderRelativeToRoot := "examples/complete"
@@ -33,7 +31,6 @@ func TestExamplesComplete(t *testing.T) {
 		VarFiles: varFiles,
 		Vars: map[string]interface{}{
 			"attributes": attributes,
-      "custom_broker_dns_name": dns_name,
 		},
 	}
 
